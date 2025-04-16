dc298ed542b61f939d3f513f043974015495f5a1
Muhammad Burhan Din <MuhammadBurhan.Din@infineon.com>
2025-04-16 15:26:47 +0800

    spring boot connection to SaaS

--- zeebespring2/src/main/java/com/camunda/Zeebespring2Application.java
+++ zeebespring2/src/main/java/com/camunda/Zeebespring2Application.java
@@ -1,12 +1,22 @@
 package com.camunda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.camunda.zeebe.client.ZeebeClient;
+
 @SpringBootApplication
 public class Zeebespring2Application implements CommandLineRunner{
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Zeebespring2Application.class);
+	
+	@Autowired
+	private ZeebeClient zeebeClient;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Zeebespring2Application.class, args);
 	}
@@ -15,7 +25,7 @@ public class Zeebespring2Application implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Running ...");
+		LOG.info("Running ... {}", zeebeClient.getConfiguration());
 		
 	}
 
